55391f4c64ec42606936191acddef0180e209227
ArunNejakar <arunnejakar847@gmail.com>
2025-12-26 21:07:47 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,4 +1,6 @@
 import sys
+from ast import literal_eval
+
 
 def bookingDecision(requestHours, priority, existingHours, capacity):
     if capacity <= 0:
@@ -15,39 +17,51 @@ def bookingDecision(requestHours, priority, existingHours, capacity):
         return "REJECTED"
 
 
-def get_camera_details(args):
+def get_camera_booking_details(args):
     # DEFAULT VALUES (Docker / Jenkins automatic run)
     cameraId = "CAM-DEFAULT"
     requestHours = 2
     priority = 1
     existingHours = 3
     capacity = 10
+    source = "DEFAULT"
 
-    # PARAMETERS FROM Jenkins / CLI
+    # PARAMETERS FROM Jenkins / CLI (ELSE CASE)
     if len(args) == 6:
         cameraId = args[1]
-        requestHours = int(args[2])
-        priority = int(args[3])
-        existingHours = int(args[4])
-        capacity = int(args[5])
+        requestHours = literal_eval(args[2])
+        priority = literal_eval(args[3])
+        existingHours = literal_eval(args[4])
+        capacity = literal_eval(args[5])
+        source = "PARAMETERS"
 
     result = bookingDecision(requestHours, priority, existingHours, capacity)
 
     return {
         "CameraID": cameraId,
-        "Status": result
+        "RequestHours": requestHours,
+        "Priority": priority,
+        "ExistingHours": existingHours,
+        "Capacity": capacity,
+        "Status": result,
+        "Source": source
     }
 
 
 def display_result(data):
     return [
+        f"Input Source   : {data['Source']}",
         f"Camera ID      : {data['CameraID']}",
-        f"Booking Status : {data['Status']}"
+        f"Request Hours : {data['RequestHours']}",
+        f"Priority      : {data['Priority']}",
+        f"Existing Hours: {data['ExistingHours']}",
+        f"Capacity      : {data['Capacity']}",
+        f"Booking Status: {data['Status']}"
     ]
 
 
-if __name__ == "__main__":
-    data = get_camera_details(sys.argv)
+if _name_ == "_main_":
+    data = get_camera_booking_details(sys.argv)
     output = display_result(data)
 
     for line in output:
